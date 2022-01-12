4bc0d184b05516eabe9ae0e37fa30fdbd6ed1e22
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-01-12 15:06:24 -0500

    styles added to welcome, character, and contract pages

--- caves-and-wyverns/src/components/Rules.jsx
+++ caves-and-wyverns/src/components/Rules.jsx
@@ -12,10 +12,12 @@ export default function Rules() {
 
   return (
     <div id="rules">
-      <h1 id="contract">Contract: The Moldscreamer!!</h1>
-      <h3><span id="help">Help needed! A monster has taken over Wind Hollow Cave! It is Gyre's only resource for precious metals and minerals. Any brave adventurer who can slay the beast will be greatly rewarded! All you need to do is go to Wind Hollow Cave and defeat the beast!</span>
-        <br />
-      <span id="directions">Directions: Depart for Wind Hollow Cave. Once you arrive in Wind Hollow Cave, click the "attack" button to roll your dice to take your chances against the fearsome Moldscreamer. If you roll higher than the Moldscreamer, the number you  rolled will be taken away from the Moldscreamer’s life force. Should you be unlucky and roll lower than the foul Moldscreamer, your lifeforce will be drained by the number rolled by the Moldscreamer. Ready to begin? Click "Go to battle!" below!</span></h3>
+      <div id="parchment">
+        <h1 id="contract">Contract: The Moldscreamer!!</h1>
+        <h3><span id="help">Help needed! A monster has taken over Wind Hollow Cave! It is Gyre's only resource for precious metals and minerals. Any brave adventurer who can slay the beast will be greatly rewarded! All you need to do is go to Wind Hollow Cave and defeat the beast!</span>
+          <br />
+        <span id="directions">Directions: Depart for Wind Hollow Cave. Once you arrive in Wind Hollow Cave, click the "attack" button to roll your dice to take your chances against the fearsome Moldscreamer. If you roll higher than the Moldscreamer, the number you  rolled will be taken away from the Moldscreamer’s life force. Should you be unlucky and roll lower than the foul Moldscreamer, your lifeforce will be drained by the number rolled by the Moldscreamer. Ready to begin? Click "Go to battle!" below!</span></h3>
+      </div>
       <button id="battle" onClick={handleClick}>Go to battle!</button>
     </div>
   )

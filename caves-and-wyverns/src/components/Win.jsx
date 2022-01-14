891f98df2844e22402358eb6041ab9d3a0203161
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-01-14 16:42:27 -0500

    fixed post it works now, deleted extra stuff that was added onto characters api

--- caves-and-wyverns/src/components/Win.jsx
+++ caves-and-wyverns/src/components/Win.jsx
@@ -1,29 +1,43 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
+// import api from '../services/apiConfig';
 
 import api3 from '../services/apiConfig/index3';
+
+// const default_input = {
+//   name: "",
+// };
 
 export default function WinOrLose() {
 
   const navigate = useNavigate();
 
   const [legends, setLegends] = useState([]);
+  const [name, setName] = useState("");
+  const [set, setSet] = useState(false);
 
   useEffect(() => {
     const fetchName = async () => {
       const res = await api3.get();
-      console.log(res.data.records.name);
-      setLegends(res.data.records.name);
+      // console.log(res.data.records);
+      setLegends(res.data.records);
     }
     fetchName();
-  }, []);
+  }, [set]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // const fields = legends;
+    const res = await api3.post("/", { fields: {name} });
+    console.log(res.data);
+    setSet(prevSet => !prevSet);
+    setName("");
+  }
 
   const handleClick = async (e) => {
     e.preventDefault();
     navigate("/");
   };
-
-  // on submit function for form goes here maybe
 
   return (
     <div>
@@ -31,18 +45,19 @@ export default function WinOrLose() {
       <h2>Add your name to the List of Legends, so your victory will always be remembered.</h2>
       <button onClick={handleClick}>Go to Home</button>
       <br />
-      <form>
+      <form
+        onSubmit={handleSubmit}>
         <label>Name: </label>
-        <input id="name" placeholder='Add your name' />
+        <input id="name" placeholder='Add your name' value={name} onChange={(e)=> setName(e.target.value)}/>
         <button>Add Name</button>
       </form>
-      {/* <div id="legends">
-        {legends.map((legend) => {
+      <div id="legends">
+        {legends.map((legend, index) => {
           return (
-            <h3>{legend.fields.name}</h3>
+            <h3 key={index}>{legend.fields.name}</h3>
           )
         })}
-      </div> */}
+      </div>
     </div>
   )
 }
--- caves-and-wyverns/src/services/apiConfig/index3.js
+++ caves-and-wyverns/src/services/apiConfig/index3.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE3 = process.env.REACT_APP_AIRTABLE_BASE3;
 const KEY3 = process.env.REACT_APP_AIRTABLE_KEY3;
 
-const baseURL = `https://api.airtable.com/v0/${BASE3}/Winners`
+const baseURL = `https://api.airtable.com/v0/${BASE3}/Winners/`
 
 const api3 = axios.create({
   baseURL,

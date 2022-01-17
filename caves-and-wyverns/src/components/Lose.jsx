cf9a31280326653f06ee93e0ef23e8c45ce70aea
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-01-17 12:53:58 -0500

    added new obit text for humor, added another media query for the lose page, edited existing media queries for lose page

--- caves-and-wyverns/src/components/Lose.jsx
+++ caves-and-wyverns/src/components/Lose.jsx
@@ -13,7 +13,7 @@ export default function Lose() {
   return (
     <div>
       <h1 id="death-notice">YOU DIED!!!!</h1>
-      <h3 id="obit">YOU ARE DEAD HAHAHAHAHAHA</h3>
+      <h3 id="obit">And so the intrepid adventurer’s body lies broken on the ground in Wind Hollow Cave. Unable to withstand the withering attacks of fungus and spore. Oh brave adventurer, who could not defeat some vicious mold. Oh heroic adventurer, unable to accomplish what some lysol and sponge could do in about 30 seconds. Oh strong adventurer, dead-- because of an overgrown portabello. I'd ask you how you feel about it, but you're dead in a dirty cave. Try again?</h3>
       <button id="dead-btn" onClick={handleClick}>Go to Home</button>
     </div>
   )
